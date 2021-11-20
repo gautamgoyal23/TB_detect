c6d2d8a5e5870135d3f18726132881f471da0890
anujsingh0796 <gautamgoyal13@gmail.com>
2021-11-20 17:18:11 +0530

    Initial commit

---
+++ temp.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from PIL import Image
+from pydantic import BaseModel
+from tensorflow.keras.models import load_model
+from typing import List
+import io
+import numpy as np
+import sys
+
+filepath = './tb_detector.h5'
+model = load_model(filepath, compile = True)
+
+input_shape = model.layers[0].input_shape
+
+app = FastAPI()
+
+class Prediction(BaseModel):
+  filename: str
+  contenttype: str
+  prediction: str
+  likely_class: int
+
+@app.get('/')
+def root_route():
+  return { 'error': 'Use GET /prediction instead of the root route!' }
+
+@app.post('/prediction/', response_model=Prediction)
+async def prediction_route(file: UploadFile = File(...)):
+
+  if file.content_type.startswith('image/') is False:
+    raise HTTPException(status_code=400, detail=f'File \'{file.filename}\' is not an image.')
+
+  try:
+    contents = await file.read()
+    pil_image = Image.open(io.BytesIO(contents))
+
+    pil_image = pil_image.resize((input_shape[1], input_shape[2]))
+
+    if pil_image.mode == 'RGBA':
+      pil_image = pil_image.convert('RGB')
+
+    if input_shape[3] and input_shape[3] == 1:
+      pil_image = pil_image.convert('L')
+
+    numpy_image = np.array(pil_image).reshape((input_shape[1], input_shape[2], input_shape[3]))
+
+    numpy_image = numpy_image / 255
+    
+    
+    prediction_array = np.array([numpy_image])
+    predictions = model.predict(prediction_array)
+    prediction = predictions[0]
+    if(prediction>0.5):
+        likely_class=1
+    else:
+        likely_class=0
+    prediction_string="You have {}% chances of having Tubercolosis".format((1-prediction)*100)
+    return {
+      'filename': file.filename,
+      'contenttype': file.content_type,
+      'prediction': prediction_string,
+      'likely_class': likely_class          
+    }
+  except:
+    e = sys.exc_info()[1]
+    raise HTTPException(status_code=500, detail=str(e))
